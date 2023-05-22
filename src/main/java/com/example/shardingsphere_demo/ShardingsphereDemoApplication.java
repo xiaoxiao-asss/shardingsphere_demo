ba86dcf0b06eeb2ddc2bfcf01c1f20298b027956
cyd <cyd@relaper.com>
2023-05-22 21:56:36 +0800

    first commit

--- src/main/java/com/example/shardingsphere_demo/ShardingsphereDemoApplication.java
+++ src/main/java/com/example/shardingsphere_demo/ShardingsphereDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShardingsphereDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("-------");
         SpringApplication.run(ShardingsphereDemoApplication.class, args);
     }
 
