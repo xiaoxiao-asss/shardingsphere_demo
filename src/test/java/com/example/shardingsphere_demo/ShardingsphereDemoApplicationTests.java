9373d0208d3ccf3d9e0918ba4a47d380d455cfa0
cyd <cyd@relaper.com>
2023-05-22 21:46:00 +0800

    first commit

---
+++ src/test/java/com/example/shardingsphere_demo/ShardingsphereDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shardingsphere_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingsphereDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
